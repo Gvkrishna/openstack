32395f281284422eed7a79e745b072d45e7fb0b0
teja624 <tejab@unifylabs.com>
2016-04-07 19:31:52 +0530

    Update cinder.rb

--- attributes/cinder.rb
+++ attributes/cinder.rb
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2014 Karol Szuster
+# Copyright (c) 2016 venkata teja
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
